be8750b1f95c94150a3faaf23723f69ab92d3738
oEg8 <hugo@woutersen.com>
2024-05-30 16:20:20 +0200

    changes

--- QLEARNclass.py
+++ QLEARNclass.py
@@ -4,6 +4,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.optimizers import Adam
 from Visualiser import Visualiser
+import tensorflow as tf
 import pickle
 import os
 import time
@@ -16,46 +17,55 @@ DOWN = 1
 LEFT = 2
 RIGHT = 3 
 
-ACTION_SIZE = 4
-WIN_THRESHOLD = 25
+"""
+TODO:
+- QLearn generiek maken. Maze env losmaken
+- Documenteren
+- Opschonen
+"""
 
 
-class Qmaze:
+
+class MazeEnv:
+    pass
+
+
+class QLearn:
     """
     This class solves randomly generated mazes using the Deep Q-Learning algorithm.
 
     """
-    def __init__(self, grid) -> None:
+    def __init__(self, grid, visualise) -> None:
         self.grid = grid
-        # self.goal = goal
-        # self.start = start
-        # self.position = [self.start[ROW], self.start[COL]]
+        self.visualise = visualise
 
-        self.step_cost = -0.01
-        self.illegal_cost = -0.05
-        self.completion_reward = 1
-        self.termination_cost = -1
+        self.step_cost = -1
+        self.illegal_cost = -3
+        self.completion_reward = 100
+        self.termination_cost = -100
         self.total_reward = 0
 
-        self.maze_completed = False
-        self.terminated = False
+        self.env_completed = False
+        self.env_terminated = False
         self.step = 0
         self.max_steps = self.grid.size * 3
 
-        self.exploration_rate = 0.9
+        self.exploration_rate = 0.1
         self.exploration_rate_decay = 0.995
         self.exploration_min = 0.01
+        self.win_threshhold = 20
 
-        self.visualiser = Visualiser()
+        if self.visualise:
+            self.visualiser = Visualiser()
 
 
     def reset(self):
-        self.position = [self.start[ROW], self.start[COL]]
+        self.position = [self.start[ROW], self.start[COL]] #
         self.state = self.calculate_state()
         self.total_reward = 0
         self.step = 0
-        self.maze_completed = False
-        self.terminated = False
+        self.env_completed = False
+        self.env_terminated = False
 
 
     def move(self, action: int) -> list:
@@ -86,9 +96,7 @@ class Qmaze:
 
 
     def calculate_state(self):
-        state = self.grid.flatten().tolist()
-        state.extend(self.position)
-        return np.array(state)
+        return np.append(self.grid.flatten(), self.position)
 
 
     def act(self, action):
@@ -117,33 +125,61 @@ class Qmaze:
         return len(self.calculate_state())
 
 
+    @classmethod
     def get_action_size(self):
-        return ACTION_SIZE
+        return 4
+    
+
+    def get_action_space(self):
+        return [0, 1, 2, 3]
 
 
     def test_for_completion(self):
-        return self.grid[self.position[ROW]][self.position[COL]]==3
+        return self.grid[self.position[ROW]][self.position[COL]] == 3
     
 
     def test_for_termination(self):
         return self.step >= self.max_steps
     
-    
-    def save_model(self, model, filename: str):
-        with open(filename, 'wb') as f:
-            pickle.dump(model, f)
-            f.close()
+
+    def learn(self, model, prev_state, action, reward, state, game_over):
+        prev_state = tf.convert_to_tensor(prev_state, dtype=tf.float32)
+        action = tf.convert_to_tensor(action, dtype=tf.int32)
+        reward = tf.convert_to_tensor(reward, dtype=tf.float32)
+        state = tf.convert_to_tensor(state, dtype=tf.float32)
+        game_over = tf.convert_to_tensor(game_over, dtype=tf.bool)
+
+        discount = 0.95
+        target_qv = model(state)
+        max_target_pv = tf.reduce_max(target_qv, axis=1)
+
+        target = reward + (discount * max_target_pv) * tf.cast(~game_over, dtype=tf.float32)
+
+        loss = self.run_gradient(model, prev_state, action, target)
+
+        return loss
 
 
-    def load_model(self, filename: str):
-        with open(filename, 'rb') as f:
-            return pickle.load(f) 
+    def run_gradient(self, model, prev_states, actions, target):
+        with tf.GradientTape(persistent=True) as tape:
+            prediction = model(prev_states)
+            
+            actions = tf.convert_to_tensor(actions, dtype=tf.int32)
+            prediction_add_action = tf.gather(prediction, actions, axis=1, batch_dims=0)
+
+            loss = tf.square((prediction_add_action - target))
+
+        grads = tape.gradient(loss, model.trainable_variables)
+        optimizer = tf.keras.optimizers.Adam()
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+        return loss
 
 
     def qtrain(self, model, **opt):
-        n_epoch = opt.get('n_epoch', 15000)
+        n_episodes = opt.get('n_episodes', 15000)
         max_memory = opt.get('max_memory', 1000)
-        data_size = opt.get('data_size', 50)
+        data_size = opt.get('data_size', 32)
         start_time = time.time()
 
         model_file = 'best_dql_solver.h5'
@@ -151,37 +187,40 @@ class Qmaze:
         if os.path.exists(model_file):
             model.load_weights(model_file)
 
-        experience = Experience(model, max_memory=max_memory)
+        memory = Experience_Replay(model, max_memory=max_memory)
 
         win_history = list()
 
-        for epoch in range(n_epoch):
+        for episodes in range(n_episodes):
             maze = MazeMaker(3, 3, 0.4, 4)
             self.grid = maze.return_maze()
             self.start = maze.return_start_coor()
             self.position = [start[0], start[1]]
 
-            start_time_epoch = time.time()
+            start_time_episode = time.time()
             loss = 0.0
             self.reset()
             game_over = False
-            epoch_cost = 0
+            episode_cost = 0
 
-            state = self.calculate_state()
+            state = self.calculate_state().reshape(1, -1)
 
-            n_episodes = 0
+            steps = 0
             while not game_over:
-                self.visualiser.draw_maze(self.grid, self.start, self.position, round(epoch_cost, 3), self.step, sum(win_history))
+                losses = []
+                if self.visualise:
+                    self.visualiser.draw_maze(self.grid, self.start, self.position, round(episode_cost, 3), self.step, sum(win_history))
                 possible_actions = self.possible_actions()
                 prev_state = state
-                if np.random.rand() < self.exploration_rate:
-                    action = np.random.choice(possible_actions)
+                if np.random.rand() < self.exploration_rate: 
+                    # action = np.random.choice(self.get_action_space())  # willekeurige acties mogen illegaal zijn
+                    action = np.random.choice(possible_actions)   # willekeurige acties mogen niet illegaal zijn
                 else:
-                    q_values = experience.predict(prev_state)
+                    q_values = model(prev_state.reshape(1, -1))
                     action = np.argmax(q_values[0])
 
                 state, reward = self.act(action)
-                epoch_cost += reward
+                episode_cost += reward
 
                 if self.test_for_completion():
                     win_history.append(1)
@@ -192,18 +231,18 @@ class Qmaze:
                 else:
                     game_over = False
 
-                episode = [prev_state, action, reward, state, game_over]
-                experience.remember(episode)
-                n_episodes += 1
+                experience = [prev_state, action, reward, state, game_over]
+                memory.remember(experience)
+                steps += 1
 
-                inputs, targets = experience.get_data(data_size=data_size)
+                if steps % 4 == 0:
+                    prev_states, actions, rewards, states, game_overs = memory.get_data(data_size=data_size)
+                    loss = self.learn(model, prev_states[-1], actions[-1], rewards[-1], states[-1], game_overs[-1])
+                    losses.append(loss)
+                    mean_loss = tf.reduce_mean(losses)
 
-                model.fit(inputs, targets, epochs=8, batch_size=16, verbose=0)
-
-                loss = model.evaluate(inputs, targets, verbose=0)
-
-                end_time_epoch = time.time()
-                epoch_time = end_time_epoch - start_time_epoch
+                end_time_episode = time.time()
+                epoch_time = end_time_episode - start_time_episode
 
             # uncomment voor een lager exploration_rate per epoch.
             # self.exploration_rate = max(self.exploration_min, self.exploration_rate * self.exploration_rate_decay)
@@ -211,14 +250,14 @@ class Qmaze:
             win_rate = sum(win_history) / len(win_history)
             end_time = time.time()
             total_time = end_time - start_time
-            template = "Epoch: {:03d}/{:d} | Loss: {:.4f} | Episodes: {:d} | Win count: {:.3f} | Win rate: {:.3f} | time (s): {:.3f} | total time (s): {:.3f}"
-            print(template.format(epoch+1, n_epoch, loss, n_episodes, sum(win_history), win_rate, epoch_time, total_time))
+            template = "Epoch: {:05d}/{:d} | Mean loss: {:07.3f} | Steps: {:02d} | Win count: {:.2f} | Win rate: {:.2f} | time (s): {:.1f} | total time (s): {:.1f}"
+            print(template.format(episodes+1, n_episodes, mean_loss, steps, sum(win_history), win_rate, epoch_time, total_time))
 
             if win_rate > 0.9:
                 self.exploration_rate = 0.5
 
-            if sum(win_history[-WIN_THRESHOLD:]) == WIN_THRESHOLD or loss == 0.0:
-                print(f"Reached 100% win rate at epoch: {epoch+1}")
+            if sum(win_history[-self.win_threshhold:]) == self.win_threshhold or loss == 0.0:
+                print(f"Reached sufficient win rate at epoch: {episodes+1}")
                 break
         
         model.save_weights('best_dql_solver.h5', True, 'h5')
@@ -231,8 +270,7 @@ class Qmaze:
         model.add(Dense(units=64, activation='relu'))
         model.add(Dense(units=self.get_action_size(), activation='linear'))
 
-        model.compile(optimizer=Adam(), loss='mse')
-
+        model.compile(optimizer=Adam())
 
         # model.summary()
         return model
@@ -243,63 +281,53 @@ class Qmaze:
         print(self.qtrain(model=model))
 
 
-class Experience:
+class Experience_Replay:
     def __init__(self, model, max_memory=1000, discount=0.95):
         self.model = model
         self.max_memory = max_memory
         self.discount = discount
         self.memory = list()
-        self.num_actions = ACTION_SIZE
+        self.num_actions = QLearn.get_action_size()
 
 
     def remember(self, episode):
         self.memory.append(episode)
         if len(self.memory) > self.max_memory:
-            del self.memory[0]
-
-
-    def predict(self, state):
-        state = np.expand_dims(state, axis=0)
-
-        return self.model.predict(state, verbose=0)
+            self.memory.pop(0)
 
 
     def get_data(self, data_size=10):
         mem_size = len(self.memory)
         data_size = min(mem_size, data_size)
-        env_size = self.memory[0][0].shape[0]
-        inputs = np.zeros((data_size, env_size))
-        targets = np.zeros((data_size, self.num_actions))
 
-        for i, idx in enumerate(np.random.choice(range(mem_size), data_size, replace=False)):
-            envstate, action, reward, envstate_next, game_over = self.memory[idx]
-            inputs[i] = envstate
-            targets[i] = self.predict(envstate)
-            Q_sa = np.max(self.predict(envstate_next))
-            if game_over:
-                targets[i, action] = reward
-            else:
-                targets[i, action] = reward + self.discount * Q_sa
+        # Determine the shapes of states
+        state_shape = self.memory[0][0].shape
 
-        return inputs, targets
+        # Initialize numpy arrays to store batches of data
+        prev_states = np.zeros((data_size,) + state_shape)
+        actions = np.zeros((data_size,), dtype=int)
+        rewards = np.zeros((data_size,))
+        states = np.zeros((data_size,) + state_shape)
+        game_overs = np.zeros((data_size,), dtype=bool)
+
+        indices = np.random.choice(range(mem_size), data_size, replace=False)
+        for i, idx in enumerate(indices):
+            prev_state, action, reward, state, game_over = self.memory[idx]
+            prev_states[i] = prev_state
+            actions[i] = action
+            rewards[i] = reward
+            states[i] = state
+            game_overs[i] = game_over
+
+        return prev_states, actions, rewards, states, game_overs
   
 
 if __name__ == '__main__':
-    # grid = np.array([[2, 0, 0], 
-    #                  [0, 0, 0], 
-    #                  [0, 0, 3]])
-    
-    # start = (0, 0)
-    # goal = (2, 2)
-
-    # Qmaze(grid, start, goal).run()
-
-
     maze = MazeMaker(3, 3, 0.4, 4)
     grid = maze.return_maze()
     start = maze.return_start_coor()
     goal = maze.return_goal_coor()
 
-    Qmaze(grid).run()
+    QLearn(grid=grid, visualise=False).run()
 
 
